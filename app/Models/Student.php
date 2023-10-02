8f6eb4abf3c7eacf4287bf81b83ed904498125f9
Thoriqaafif <thoriq.afif.habibi@gmail.com>
2023-10-03 06:14:58 +0700

    add model and controller

---
+++ app/Models/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+
+class Student extends Model implements Authenticatable
+{
+    use HasFactory;
+    use AuthenticatableTrait;
+
+    protected $fillable=[
+        'name',
+        'email',
+        'class',
+        'dateOfBirth',
+        'profilePicture'
+    ];
+}
